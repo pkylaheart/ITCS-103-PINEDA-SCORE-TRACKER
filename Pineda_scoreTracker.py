20ee4746d5dba49a62402aa8b6d6ed07dee34f5d
pkylaheart <125124933+pkylaheart@users.noreply.github.com>
2025-05-02 08:37:45 +0800

    updated

--- Pineda_scoreTracker.py
+++ Pineda_scoreTracker.py
@@ -3,11 +3,6 @@ import tkinter as tk
 from tkinter import ttk
 from openpyxl import Workbook, load_workbook
 
-wb = Workbook()
-ws = wb.active
-ws.title = "Score Tracker"
-ws.append(["Name", "Score"])
-
 
 def create_excel():
     if not os.path.exists("student_scores.xlsx"):
@@ -16,6 +11,25 @@ def create_excel():
         ws.title = "Score Tracker"
         ws.append(["Name", "Score", "Remarks"])  
         wb.save("student_scores.xlsx")
+
+def display_data():
+    if not os.path.exists("student_scores.xlsx"):
+        print("Excel file does not exist.")
+        return
+
+    wb = load_workbook("student_scores.xlsx")
+    sheet = wb.active
+
+    for item in treeview.get_children():
+        treeview.delete(item)
+
+    list_values = list(sheet.values)
+
+    for col_index, col_name in enumerate(list_values[0]):
+        treeview.heading(cols[col_index], text=col_name)
+
+    for value_tuple in list_values[1:]:
+        treeview.insert('', tk.END, values=value_tuple)
 
 def submit():
     name = entry_name.get()
@@ -41,19 +55,18 @@ def submit():
         wb.save("student_scores.xlsx")
         print("Added Successfully.")
 
-        notif.config(text = "Submitted", foreground = "green")
+        notif.config(text = "Submitted", font = ("Arial", 10), foreground = "white")
         entry_name.delete(0, tk.END)
         entry_score.delete(0, tk.END)
         entry_name.focus()
         app.after(2000, lambda: notif.config(text = ""))
-
+        display_data()
     else:
         print("Fields Left Blank")
 
 app = tk.Tk()
 app.tk.call('source', 'forest-dark.tcl')
 ttk.Style().theme_use('forest-dark')
-app.geometry("300x300")
 app.title("Score Tracker")
 app.resizable(False, False)
 create_excel()
@@ -83,5 +96,20 @@ button.grid(row = 5, column=0, sticky="nsew" )
 
 notif = ttk.Label(app, text="", font=("Arial", 12))
 notif.pack(pady=10)
+
+show_excel = ttk.Frame (frame)
+show_excel.grid(row = 0, column = 1, pady = 10)
+show_scroll = ttk.Scrollbar(show_excel)
+show_scroll.pack(side = "right", fill = "y")
+
+cols = ("Name", "Score", "Remarks")
+treeview = ttk.Treeview(show_excel, show = "headings", yscrollcommand=show_scroll.set, columns = cols, height = 13)
+treeview.column("Name", width=100)
+treeview.column("Score", width=50)
+treeview.column("Remarks", width=100)
+treeview.pack()
+show_scroll.config(command = treeview.yview)
+
+display_data()
 
 app.mainloop()
